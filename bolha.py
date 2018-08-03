a23ab1942c698a09e547fefbdbaccf41c7ee5c48
Leonardo Gomes <leonardo.gomes@lit.ifce.edu.br>
2018-08-03 17:17:28 -0300

    atualizzando

--- bolha.py
+++ bolha.py
@@ -3,18 +3,20 @@ from random import randint
 import timeit
 import numpy as np
 import scipy.interpolate as interpolate
+from threading import Thread
 
-def desenhaGraficoSuave(x,y,xl = "Nº de Elementos", yl = "Tempo(s)"):
+def desenhaGraficoSuave(x,y,l, n, xl = "Nº de Elementos", yl = "Tempo(s)"):
     xnew = np.linspace(min(x), max(x), 10 * (max(x) - min(x)))
     a, b, c = interpolate.splrep(x, y, s=0, k=2)
     suave = interpolate.BSpline(a, b, c, extrapolate=False)
+    plt.subplot(n)
     plt.plot(xnew, suave(xnew), label="Curva Suave")
     plt.plot(x,y, label="Curva Sem Suaveização")
     plt.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
     plt.ylabel(yl)
     plt.xlabel(xl)
-    plt.title('Suavizacao', fontsize=18)
-    plt.show()
+    plt.title(l, fontsize=12)
+    
 
 def geraLista(tam):
     lista = []
@@ -35,7 +37,7 @@ def listaDecrescente(tam):
     while (tam > 0):
         lista.append(tam)
         tam = tam-1
-    print (lista)
+    return lista
 
 def ordena(vector):
     tam = len(vector)
@@ -57,18 +59,44 @@ def ordena(vector):
 
 nums = [1000,10000,20000,30000,40000,50000,60000,70000,80000,90000,100000]
 #nums = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
-time = []
 
-for r in nums:
-    vector = geraLista(r)
-    tempo = timeit.timeit("ordena({})".format(vector),setup="from __main__ import ordena",number=1)
-    time.append(tempo)
-    print(time)
+def casoMedio(nums1):
+	nums = nums1
+	time = []
+	for r in nums:
+	    print(r)
+	    vector = geraLista(r)
+	    tempo = timeit.timeit("ordena({})".format(vector),setup="from __main__ import ordena",number=1)
+	    time.append(tempo)
 
-desenhaGraficoSuave(nums, time, "Nº de Elementos", "Tempo(s)")
+	desenhaGraficoSuave(nums, time,'Caso Medio',211, "Nº de Elementos", "Tempo(s)")
 
+def piorCaso(nums1):
+	nums=nums1
+	time1=[]
+	for r in nums:
+		print (r)
+		vector1 = listaDecrescente(r)
+		tempo = timeit.timeit("ordena({})".format(vector1),setup="from __main__ import ordena",number=1)
+		time1.append(tempo)
 
+	desenhaGraficoSuave(nums, time1, 'Pior Caso',212, "Nº de Elementos", "Tempo(s)")
 
+'''
+if __name__ == '__main__':
+
+	ordena1 = Thread(target=casoMedio,args=[nums])
+	ordena2 = Thread(target=piorCaso,args=[nums])
+
+	ordena1.start()
+	ordena2.start()
+
+	plt.show()
+'''
+casoMedio(nums)
+piorCaso(nums)
+
+plt.show()
 #print(tempo)
 
 #print(ordena(vector))
---
+++ select
@@ -0,0 +1,9 @@
+def geraLista(tam):
+    lista = []
+    while(len(lista) < tam):
+        n = randint(1,100)
+        lista.append(n)
+        #if n not in lista: lista.append(n)
+    return lista
+
+    
