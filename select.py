0cf049592ab176283d995ddbf66f9fcb6a7b717f
Leonardo Gomes <leonardo.gomes@lit.ifce.edu.br>
2018-08-08 07:18:30 -0300

    adding fotos

--- select.py
+++ select.py
@@ -53,7 +53,7 @@ def selectionSort(vetor):
 	return vetor
 
 numsT = [1000,10000,20000,30000,40000,50000,60000,70000,80000,90000,100000]
-#numsT = [10, 100, 200]
+
 
 def casoMedio(nums0):
 	nums = nums0
